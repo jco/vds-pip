ef35ef1136e9b03ffef479f54120daf2c1f516d2
jco <jeff.jtcox@gmail.com>
2011-07-22 15:52:38 -0400

    Fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VdsPip::Application.config.secret_token = '741390ccc2602a6929763b355b692d6d15c1ac45b1e2ca9d6f5884f58c4fc439e54039f88f463cf04c534682ca7493c1502a6c65c423df84fbf9d6687a00dc8b'
